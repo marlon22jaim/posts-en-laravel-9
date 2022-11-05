9e459d79c3215ce6ab8986d703df0a2526d2d27b
Marlon Jaimes <andres_jr19@hotmail.com>
2022-11-05 16:56:23 -0500

    iniciando rutas del proyecto

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -12,7 +12,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+// get
+Route::view("/", "welcome")->name("home");
+Route::view("/contact", "contact")->name("contact");
+Route::view("/blog", "blog")->name("blog");
+Route::view("/about", "about")->name("about");

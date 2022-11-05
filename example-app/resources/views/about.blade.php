a34f2e8431b92ba088183e924b4820b884239208
Marlon Jaimes <andres_jr19@hotmail.com>
2022-11-05 18:42:57 -0500

    agregando componentes de blade a la navegació

--- example-app/resources/views/about.blade.php
+++ example-app/resources/views/about.blade.php
@@ -1,15 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
+<x-layouts.app title="About" metaDescription="Descripcion About">
     <h1>About</h1>
-</body>
-
-</html>
+</x-layouts.app>
--- example-app/resources/views/welcome.blade.php
+++ example-app/resources/views/welcome.blade.php
@@ -1,21 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Inicio</title>
-</head>
-
-<body>
-    <ul>
-        <li><a href="<?= route("home") ?>">Home</a></li>
-        <li><a href="<?= route("blog") ?>">Blog</a></li>
-        <li><a href="<?= route("about") ?>">About</a></li>
-        <li><a href="<?= route("contact") ?>">Contact</a></li>
-    </ul>
+<x-layouts.app title="Home" metaDescription="Descripcion Home">
     <h1>Inicio</h1>
-</body>
-
-</html>
+</x-layouts.app>

47abd3307aa0434c0a76f2c3484783a202869aa1
Marlon Jaimes <andres_jr19@hotmail.com>
2022-11-05 19:13:28 -0500

    creando el primer controlador de prueba

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,3 +1,6 @@
 <x-layouts.app title="Blog" metaDescription="Descripcion Blog">
     <h1>Blog</h1>
+    @foreach ($posts as $post)
+        <h2>{{ $post['title'] }}</h2>
+    @endforeach
 </x-layouts.app>
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,10 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-// get
 Route::view("/", "welcome")->name("home");
 Route::view("/contact", "contact")->name("contact");
-Route::view("/blog", "blog")->name("blog");
+
+Route::get("/blog", [PostController::class,"index"])->name("blog");
 Route::view("/about", "about")->name("about");

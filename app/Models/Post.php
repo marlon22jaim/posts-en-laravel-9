6c1ffc143f5fa66665a7ca78abbcf7462e772694
Marlon Jaimes <andres_jr19@hotmail.com>
2022-11-06 15:46:17 -0500

    usando ORM

---
+++ app/Models/Post.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    //si queremos cambiarle el nombre a la tabla asociada
+    // protected $table = "posts";
+}

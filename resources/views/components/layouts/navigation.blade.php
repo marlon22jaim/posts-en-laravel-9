664864217cd6fddd2e73b41fe4f9f28d22eaa861
Marlon Jaimes <andres_jr19@hotmail.com>
2022-11-09 01:27:12 -0500

    terminando los estilos de la barra de navegacion

--- resources/views/components/layouts/navigation.blade.php
+++ resources/views/components/layouts/navigation.blade.php
@@ -1,18 +1,9 @@
-{{-- <ul>
-    <li><a href="">Home</a></li>
-    <li><a href="{{ route('posts.index') }}">Blog</a></li>
-    <li><a href="{{ route('about') }}">About</a></li>
-    <li><a href="{{ route('contact') }}">Contact</a></li>
-</ul> --}}
-
-
-<nav
-    class="w-screen overflow-scroll bg-white border-b dark:bg-slate-900 border-slate-900/10 lg:px-8 dark:border-slate-300/10 lg:mx-0">
+<nav class="w-screen bg-white border-b border-slate-900/10 lg:px-8 dark:border-slate-300/10 lg:mx-0">
     <div class="px-4 mx-auto max-w-7xl sm:px-16 lg:px-20">
         <div class="relative flex items-center justify-between h-16">
             <div class="flex items-center justify-center flex-1 sm:items-stretch sm:justify-start">
                 <div class="flex items-center flex-shrink-0">
-                    <a href="#">
+                    <a href="{{ route('home') }}">
                         <svg class="w-8 h-8 text-sky-500" fill="none" width="0" stroke="currentColor"
                             viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
@@ -29,22 +20,22 @@
                     <div class="flex space-x-4">
                         <!-- Active: 'text-sky-600 dark:text-white', Inactive 'text-slate-400' -->
                         <a href="{{ route('home') }}"
-                            class="px-3 py-2 text-sm font-medium rounded-md hover:text-sky-600 dark:hover:text-white">
+                            class="px-3 py-2 text-sm font-medium rounded-md hover:text-sky-600 {{ request()->routeIs('home') ? 'text-sky-600' : 'text-slate-400' }}">
                             Home
                         </a>
                         <!-- Active: 'text-sky-600 dark:text-white', Inactive 'text-slate-400' -->
-                        <a href="#"
-                            class="px-3 py-2 text-sm font-medium rounded-md hover:text-sky-600 dark:hover:text-white">
+                        <a href="{{ route('posts.index') }}"
+                            class="px-3 py-2 text-sm font-medium rounded-md hover:text-sky-600 {{ request()->routeIs('posts.*') ? 'text-sky-600' : 'text-slate-400' }}">
                             Blog
                         </a>
                         <!-- Active: 'text-sky-600 dark:text-white', Inactive 'text-slate-400' -->
-                        <a href="#"
-                            class="px-3 py-2 text-sm font-medium rounded-md hover:text-sky-600 dark:hover:text-white">
+                        <a href="{{ route('about') }}"
+                            class="px-3 py-2 text-sm font-medium rounded-md hover:text-sky-600 {{ request()->routeIs("about") ? 'text-sky-600'  : 'text-slate-400'}}">
                             About
                         </a>
                         <!-- Active: 'text-sky-600 dark:text-white', Inactive 'text-slate-400' -->
-                        <a href="#"
-                            class="px-3 py-2 text-sm font-medium rounded-md hover:text-sky-600 dark:hover:text-white">
+                        <a href="{{ route('contact') }}"
+                            class="px-3 py-2 text-sm font-medium rounded-md hover:text-sky-600 {{ request()->routeIs("contact") ? 'text-sky-600'  : 'text-slate-400'}}">
                             Contact
                         </a>
                     </div>
@@ -53,3 +44,7 @@
         </div>
     </div>
 </nav>
+
+
+
+
